d706fa9e7577d48237a5e47dac817a3db274548f
auriceliomatos <auriceliomatos@gmail.com>
2024-12-28 14:30:59 -0300

    doc realizado

---
+++ src/App.vue
@@ -0,0 +1,116 @@
+<script setup>
+import { computed, ref } from "vue";
+import foot from './components/foot.vue';
+
+
+// Declaração de variáveis reativas
+const num1 = ref(0);
+const num2 = ref(0);
+const operation = ref("+");
+
+// Computação do resultado
+const result = computed(() => {
+  switch (operation.value) {
+    case "+":
+      return num1.value + num2.value;
+    case "-":
+      return num1.value - num2.value;
+    case "*":
+      return num1.value * num2.value;
+    case "/":
+      return num2.value !== 0 ? num1.value / num2.value : "Erro: Divisão por zero";
+    default:
+      return "Operação inválida";
+  }
+});
+</script>
+
+<!-------------------------------------- template --------------------------------->
+
+<template>
+  <div id="app" class="container">
+  <h1>Calculadora do Automatica</h1>
+
+    <img src="@/assets/r-1.jpg" alt="Robô Infantil" class="robot-image" />
+    
+    <div class="calculator">
+      <input
+        type="number"
+        v-model.number="num1"
+        placeholder="Digite o primeiro número"
+      />
+      <select v-model="operation">
+        <option value="+">Somar (+)</option>
+        <option value="-">Subtrair (-)</option>
+        <option value="*">Multiplicar (*)</option>
+        <option value="/">Dividir (/)</option>
+      </select>
+      <input
+        type="number"
+        v-model.number="num2"
+        placeholder="Digite o segundo número"
+      />
+      <p class="result">Resultado: {{ result }}</p>
+         <foot />
+    </div>
+  </div>
+</template>
+
+<!--------------------------------------- style ---------------------------------------->
+
+<style scoped>
+
+.container {
+  max-width: 500px;
+
+  padding: 10px;
+  text-align: center;
+  background-color: #f9f9f9;
+  border: 2px solid #e0e0e0;
+  border-radius: 10px;
+  box-shadow: 0px 4px 10px rgba(0, 0, 0, 0.1);
+  display: flex;
+
+}
+.robot-image {
+  width: 200px;
+  margin: 0px;
+  padding: 10px;
+  
+  
+}
+
+.calculator {
+  display: flex;
+  flex-direction: column;
+  gap: 10px;
+}
+
+input,
+select {
+  padding: 10px;
+  font-size: 16px;
+  border: 1px solid #ccc;
+  border-radius: 5px;
+}
+
+input:focus,
+select:focus {
+  outline: none;
+  border-color: #007bff;
+  box-shadow: 0 0 5px rgba(0, 123, 255, 0.5);
+}
+
+.result {
+  margin-top: 20px;
+  font-size: 18px;
+  font-weight: bold;
+  color: #007bff;
+}
+
+h1 {
+  font-size: 24px;
+  color: #333;
+  margin-bottom: 20px;
+}
+</style>
---
+++ src/components/foot.vue
@@ -0,0 +1,53 @@
+<template>
+    <footer id="social-links" class="p-5 mb-4 mt-4 rounded-3">
+        <div class="nos-acompanhe">
+            <ul class="social-links">
+             
+                <li>
+                    <a href="https://github.com/auriceliomatos" title="Siga  no GitHub">
+                        <img src="/src/img/github-icon.png" alt=" GitHub" />
+                    </a>
+                </li>
+            </ul>
+            <p>
+               Auricélio Matos
+            </p>
+        </div>
+        <div>
+            <p> &copy; calculadora automatica </p>
+        </div>
+    </footer>
+</template>
+
+<style scoped>
+footer {
+    background-color: #27ae60;
+    color: white;
+    text-align: center;
+    padding: 16px 0;
+    margin-top: 32px;
+    width: 700px;
+}
+
+.nos-acompanhe {
+    display: flex;
+    justify-content: center;
+    margin-top: 20px;
+    margin-bottom: 20px;
+    align-items: center;
+}
+
+.social-links img {
+    height: 50px;
+    margin-right: 8px;
+}
+
+.social-links li {
+    display: inline;
+    margin-right: 8px;
+}
+
+.social-links li a {
+    text-decoration: none;
+}
+</style>
